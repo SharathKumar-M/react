0a1cdcd2cb08c7d6aeb5ee0996649f461e86bdac
SHARATH KUMAR.M <sharathm6667@gmail.com>
2025-12-30 21:47:52 +0530

    small changes

--- react-movie/src/App.jsx
+++ react-movie/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
       <div className="wrapper"></div>
       <header>
         <img src="./hero.png" alt="Hero Banner" />
-        <h1>Find the <span className="text-gradient">Movies</span> You'll Enjoy Without Hussle</h1>
+        <h1>Find the <span className="text-gradient">Movies</span> You'll Enjoy </h1>
       </header>
 
       <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
