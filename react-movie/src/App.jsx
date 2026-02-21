ea7512f35623395d49ecb4bb9c19b278b61fddd7
SHARATH KUMAR.M <sharathm6667@gmail.com>
2026-02-21 12:21:22 +0530

    search component

--- react-movie/src/App.jsx
+++ react-movie/src/App.jsx
@@ -9,12 +9,22 @@ const App = () => {
       <div className="pattern"></div>
 
       <div className="wrapper"></div>
-      <header>
-        <img src="./hero.png" alt="Hero Banner" />
-        <h1>Find the <span className="text-gradient">Movies</span> You'll Enjoy without any Hussle </h1>
-      </header>
+      <header className="relative h-screen flex flex-col items-center justify-center bg-cover bg-center"
+  style={{ backgroundImage: "url('/hero.bg.png')" }}
+>
+  <img
+    src="/hero.png"
+    alt="Hero Banner"
+    className="relative z-10"
+  />
+
+  <h1 className="relative z-10 text-white text-4xl text-center">
+    Find the <span className="text-gradient">Movies</span> You'll Enjoy without any Hussle
+  </h1>
+</header>
 
       <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <h1 className="text-white">{searchTerm}</h1>
     </main>
   )
 }
--- react-movie/src/components/Search.jsx
+++ react-movie/src/components/Search.jsx
@@ -1,6 +1,6 @@
-import react from 'react';
+import React from 'react';
 
-    const Search = ([searchTerm, setSearchTerm]) => {
+    const Search = ({searchTerm, setSearchTerm}) => {
         return (
             <div className='search'>
                 <div>
