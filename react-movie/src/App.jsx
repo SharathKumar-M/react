0068be39aaa667091724cddd00ec806e8e0acb8a
SHARATH KUMAR.M <sharathm6667@gmail.com>
2026-06-17 22:37:10 +0530

    Fix typo in header text

--- react-movie/src/App.jsx
+++ react-movie/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   />
 
   <h1 className="relative z-10 text-white text-4xl text-center">
-    Find the <span className="text-gradient">Movies</span> You'll Enjoy withut @ny Hussle
+    Find the <span className="text-gradient">Movies</span> You'll Enjoy withut Any Hussle
   </h1>
 </header>
 
